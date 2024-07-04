f1f108c5fe4ecb92f89330968f68eaab7ec1c91f
PietroMC <xdexco@gmail.com>
2024-07-04 16:34:51 +0200

    updated scripts

--- script_pc_lorenzo.sh
+++ script_pc_lorenzo.sh
@@ -18,7 +18,7 @@ run_a_table_column()
     local testa_e_butta=$0
 
     ## CRD
-    # docker run -v $PWD/torchcam_lib:/work/project/   --gpus all --ipc host -u 1001:1001 piemmec/xai4kd_2:2 /usr/bin/python3 /work/project/train_student_xai.py --model_t $1 --distill crd --model_s $2 -c 1 -d 1 -b 0.8 --trial 0 --gpu_id $4 --batch_size 8 --dataset imagenette  --xai noXAI --w_xai 0
+    docker run -v $PWD/torchcam_lib:/work/project/   --gpus all --ipc host -u 1001:1001 piemmec/xai4kd_2:2 /usr/bin/python3 /work/project/train_student_xai.py --model_t $1 --distill crd --model_s $2 -c 1 -d 1 -b 0.8 --trial 0 --gpu_id $4 --batch_size 8 --dataset imagenette  --xai noXAI --w_xai 0
     docker run -v $PWD/torchcam_lib:/work/project/   --gpus all --ipc host -u 1001:1001 piemmec/xai4kd_2:2 /usr/bin/python3 /work/project/train_student_xai.py --model_t $1 --distill crd --model_s $2 -c 1 -d 1 -b 0.8 --trial 0 --gpu_id $4 --batch_size 8 --dataset imagenette  --xai $3 --w_xai 10
     docker run -v $PWD/torchcam_lib:/work/project/   --gpus all --ipc host -u 1001:1001 piemmec/xai4kd_2:2 /usr/bin/python3 /work/project/train_student_xai.py --model_t $1 --distill crd --model_s $2 -c 1 -d 1 -b 0.8 --trial 0 --gpu_id $4 --batch_size 8 --dataset imagenette  --xai $3 --w_xai 50
     docker run -v $PWD/torchcam_lib:/work/project/   --gpus all --ipc host -u 1001:1001 piemmec/xai4kd_2:2 /usr/bin/python3 /work/project/train_student_xai.py --model_t $1 --distill crd --model_s $2 -c 1 -d 1 -b 0.8 --trial 0 --gpu_id $4 --batch_size 8 --dataset imagenette  --xai $3 --w_xai 100
